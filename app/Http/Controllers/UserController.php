d746d4f57e96107cca1ac2ba4ba044b066214ebb
Ana Suryana di PSI <ana.suryana@smt.co.id>
2024-07-11 07:14:46 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $dataReq = $request->json()->all();        
+        $data = [
+            'nick_name' => $dataReq['username'],
+            'password' => $dataReq['password'],
+            'active' => '1',
+        ];
+
+        if (Auth::attempt($data)) {
+            $user = User::where('nick_name', $dataReq['username'])->first();
+            $user->token = $user->createToken($dataReq['password'] . 'bebas')->plainTextToken;
+            return $user;
+        } else {
+            throw new HttpResponseException(response([
+                'errors' => [
+                    'message' => [
+                        'username or password wrong'
+                    ], 'data' => $data
+                ]
+            ], 401));
+        }
+    }
+}
