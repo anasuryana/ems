317b8f23e77eb6639607fe0587f0020c73b799cc
Ana Suryana di PSI <ana.suryana@smt.co.id>
2024-08-21 10:58:50 +0700

    feat : log viewers

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//log-viewers
+Route::get('log-viewers', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
 
 
