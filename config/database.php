e8a69c1c971964c2568d7204ef1246188b820759
Ana Suryana di PSI <ana.suryana@smt.co.id>
2025-09-17 08:41:14 +0700

    fix : tambah koneksi database

--- config/database.php
+++ config/database.php
@@ -163,6 +163,21 @@ return [
             ]) : [],
         ],
 
+        'sqlsrv_it_inventory' => [
+            'driver' => 'sqlsrv',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '1433'),
+            'database' => 'PSI_RPCUST',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'true'),
+        ],
+
     ],
 
     /*
