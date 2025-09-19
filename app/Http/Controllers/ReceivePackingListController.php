4d9addf039e9006f8245c0b795e0f3080a10b0d9
Ana Suryana di PSI <ana.suryana@smt.co.id>
2025-09-19 10:51:06 +0700

    feat : support upload template omi

--- app/Http/Controllers/ReceivePackingListController.php
+++ app/Http/Controllers/ReceivePackingListController.php
@@ -44,10 +44,14 @@ class ReceivePackingListController extends Controller
                 // periksa tipe template
                 $templateType = 1;
                 if ($sheet->getCell('A3')->getCalculatedValue()) {
-                    $templateType = 2;
+                    if (str_contains(strtolower($sheet->getCell('A2')->getCalculatedValue()), 'omron')) {
+                        $templateType = 3;
+                    } else {
+                        $templateType = 2;
+                    }
                 }
 
-                if ($templateType == 1) {
+                if ($templateType === 1) {
                     $DONumberHeader = $sheet->getCell('X7')->getCalculatedValue();
                     if (empty($DONumberHeader)) {
                         $notStandardList[] = [
@@ -110,7 +114,7 @@ class ReceivePackingListController extends Controller
 
                         $rowIndex += 2;
                     }
-                } else {
+                } elseif ($templateType === 2) {
                     $rowIndex = 4;
 
                     while (!empty($sheet->getCell('F' . $rowIndex)->getCalculatedValue())) { // patokan dari kolom F                
@@ -145,9 +149,47 @@ class ReceivePackingListController extends Controller
                         $rowIndex++;
                         $DONumber[] = $_do_number;
                     }
+                } elseif ($templateType === 3) {
+                    $rowIndex = 8;
+                    $DONumberHeader = $sheet->getCell('C8')->getCalculatedValue();
+                    $DONumber[] = $DONumberHeader;
+                    $_date = trim($sheet->getCell('B8')->getValue());
+                    if (empty($_date)) {
+                        $notStandardList[] = [
+                            'case' => 'date',
+                            'message' => 'Date on line ' . $rowIndex . ' is empty',
+                            'file' => $fileName
+                        ];
+                        $rowIndex++;
+                        continue;
+                    }
+                    $_date_o = \Carbon\Carbon::instance(Date::excelToDateTimeObject($_date));
+
+                    while (!empty($sheet->getCell('E' . $rowIndex)->getCalculatedValue()) && !empty($sheet->getCell('F' . $rowIndex)->getCalculatedValue())) { // patokan dari kolom F                
+                        $_pallet = '';
+
+                        $_qty = str_replace(',', '', trim($sheet->getCell('G' . $rowIndex)->getCalculatedValue()));
+                        $data[] = [
+                            'delivery_doc' => $DONumberHeader,
+                            'created_by' => $request->user_id,
+                            'created_at' => date('Y-m-d H:i:s'),
+                            'item_code' => trim($sheet->getCell('E' . $rowIndex)->getCalculatedValue()),
+                            'delivery_date' => $_date_o->format('Y-m-d'),
+                            'delivery_quantity' => $_qty,
+                            'ship_quantity' => $_qty,
+                            'pallet' => $Pallet,
+                            'item_name' => ''
+                        ];
+
+                        $rowIndex++;
+                    }
+                } else {
+                    $notStandardList[] = [
+                        'case' => 'format',
+                        'message' => 'currently the template is not supported',
+                        'file' => $fileName
+                    ];
                 }
-
-
 
                 if ($data && empty($notStandardList)) {
                     try {
@@ -158,7 +200,23 @@ class ReceivePackingListController extends Controller
 
                         $TOTAL_COLUMN = 8;
                         $insert_data = collect($data);
-                        $chunks = $insert_data->chunk(2000 / $TOTAL_COLUMN);
+
+                        $resume = $insert_data->groupBy('item_code')->map(function ($items, $itemCode) {
+                            return [
+                                'delivery_doc' => $items->first()['delivery_doc'],
+                                'created_by' => $items->first()['created_by'],
+                                'created_at' => date('Y-m-d H:i:s'),
+                                'item_code' => $itemCode,
+                                'delivery_date' => $items->first()['delivery_date'],
+                                'delivery_quantity' => $items->sum('delivery_quantity'),
+                                'ship_quantity' => $items->sum('ship_quantity'),
+                                'item_name' => '',
+                                'pallet' => ''
+                            ];
+                        })->values();
+
+
+                        $chunks = $resume->chunk(2000 / $TOTAL_COLUMN);
                         foreach ($chunks as $chunk) {
                             DB::connection('sqlsrv_wms')->table('receive_p_l_s')->insert($chunk->toArray());
                         }
